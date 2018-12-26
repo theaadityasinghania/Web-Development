83d99351c42c0fb88ae5d25b65fb16dd88c9fe5d
theaadityasinghania <theaadityasinghania@gmail.com>
2018-12-26 18:47:12 +0530

    JavaScript Introduction.

---
+++ javascript1/scripts/main.js
@@ -0,0 +1,44 @@
+// function go(){
+// 	alert('Hi');
+// 	alert('Hey there!');
+// }
+
+// go();
+
+var myList = ['apples', 'oranges', 'bananas']
+
+
+myList.forEach(function(value, index) {
+	console.log(value, index);
+	alert("I have "+index+" number of "+value+" with me.")
+})
+
+console.log(myList.length)
+// myList[0] = 'watermaelon'
+
+// var dataset = ['apples', 12, go]
+
+// dataset[3] = ['Aaditya','Singhania']
+
+// console.log(myList)
+
+// myList.shift()
+
+// console.log(myList)
+
+// var curritem = myList.shift()
+
+// console.log(curritem)
+// console.log(myList)
+
+// console.log(myList)
+
+// myList.pop()
+
+// console.log(myList)
+
+// var curritem = myList.pop()
+
+// console.log(curritem)
+// console.log(myList)
+
